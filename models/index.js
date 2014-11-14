e0ea1ebe79599e5587a49f1c2b8f8b5f987c122d
Arnaud SAMSON <nono.samson@gmail.com>
2014-11-14 16:17:12 -0500

    Add model index

---
+++ models/index.js
@@ -0,0 +1,5 @@
+/*jslint node: true, sloppy: true*/
+exports.Carac = require('./carac');
+exports.Part = require('./part');
+exports.PartType = require('./partType');
+exports.StokageLocation = require('./stokageLocation');
