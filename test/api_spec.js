793ca64145df957dc672371f33cbc086843f3aaa
Arnaud SAMSON <nono.samson@gmail.com>
2014-11-21 16:07:01 -0500

    Unit test pour Location

--- test/api_spec.js
+++ test/api_spec.js
@@ -2,22 +2,64 @@
 var frisby = require('frisby');
 var crypto = require('crypto');
 
-var md5sum = crypto.createHash('md5');
-md5sum.update(Math.random() * 1000);
+var md5sum = crypto.createHash('md5').update((Math.random() * 1000).toString()).digest('hex');
+var md5sum2 = crypto.createHash('md5').update((Math.random() * 1000).toString()).digest('hex');
 
-
-frisby.create('Post the /locations locations')
+//test block for Location route
+//Post
+var postTest = frisby.create('Post the /locations locations')
   .post('http://localhost:3000/locations', {
     location: md5sum
-  })
+  }, {json: true})
   .expectStatus(200)
   .expectHeaderContains('content-type', 'application/json')
-  .expectJSON('0', {
-    '_id': null,
+  .expectJSON({
     location: md5sum
   })
-  .expectJSONTypes('0', {
-    '_id': null,
+  .expectJSONTypes({
+    '_id': String,
+    location: String,
+    created: String,
+    modified: String,
+    '__v': Number
+  });
+
+//Put
+postTest.afterJSON(function (json) {
+  frisby.create('Put the /locations route')
+    .put('http://localhost:3000/locations/' + json._id, {
+      location: md5sum2
+    }, {json: true})
+    .expectStatus(200)
+    .expectHeaderContains('content-type', 'application/json')
+    .expectJSON({
+      location: md5sum2
+    })
+    .expectJSONTypes({
+      '_id': String,
+      location: String,
+      created: String,
+      modified: String,
+      '__v': Number
+    })
+    .toss();
+});
+//Delete
+postTest.afterJSON(function (json) {
+  frisby.create('Delet the /locations route')
+    .delete('http://localhost:3000/locations/' + json._id, {
+      location: md5sum2
+    }, {json: true})
+    .expectStatus(200)
+    .toss();
+}).toss();
+//Get
+frisby.create('Get the /locations route')
+  .get('http://localhost:3000/locations')
+  .expectStatus(200)
+  .expectHeaderContains('content-type', 'application/json')
+  .expectJSONTypes('*', {
+    '_id': String,
     location: String,
     created: String,
     modified: String,
@@ -25,9 +67,4 @@ frisby.create('Post the /locations locations')
   })
   .toss();
 
-frisby.create('Get the /locations route')
-  .get('http://localhost:3000/locations')
-  .expectStatus(200)
-  .expectHeaderContains('content-type', 'application/json')
-  .expectJSON('0', null)
-  .toss();
+
